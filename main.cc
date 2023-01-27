9a136ee5594fd934e6da38183ee6ff7e14112e17
daniel yu <wacfeldwang333@gmail.com>
2023-01-27 10:52:07 -0500

    image viewing and panning

--- main.cc
+++ main.cc
@@ -4,11 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <vector>
 
-#define error(fmt, ...) do { fprintf(stderr, "%s: %d: %s: " fmt, __FILE__, __LINE__, __func__ __VA_OPT__(,) __VA_ARGS__); getchar();/*close(); exit(1);*/ } while(0)
+#define error(fmt, ...) do { endwin(); close(); fprintf(stderr, "%s: %d: %s: " fmt, __FILE__, __LINE__, __func__ __VA_OPT__(,) __VA_ARGS__); exit(1);} while(0)
 
-const int SCREEN_WIDTH = 500;
-const int SCREEN_HEIGHT = 500;
+#define SCREEN_WIDTH 700
+#define SCREEN_HEIGHT 500
 
 SDL_Window *gwin;
 SDL_Surface *gsurf;
@@ -62,24 +63,87 @@ void init(const char *title, int w, int h)
   }
 }
 
-int main(int argc, char **args)
+int main(int argc, char **argv)
 {
+	// while(true)
+	// {
+	//   c = getch();
+	//   move(0,0);
+	//   clrtoeol();
+	//   printw("%d", c);
+	//   // refresh();
+	// }
 
+  if(argc == 1)
+  {
+    error( "no images provided\n");
+    return 0;
+  }
+  
+  // init SDL2 window
+  init("tetrui", SCREEN_WIDTH, SCREEN_HEIGHT);
+  SDL_FillRect( gsurf, NULL, SDL_MapRGB( gsurf->format, 0xFF, 0xFF, 0xFF ) );
+  SDL_UpdateWindowSurface( gwin );
+
+  // load images
+  // https://wiki.libsdl.org/SDL2/SDL_Surface
+  std::vector<SDL_Surface *> imgs;
+  for(int i = 1; i < argc; i++)
+  {
+    printf("loading %s\n", argv[i]);
+    SDL_Surface *s = loadBMP(argv[i]);
+    imgs.push_back(s);
+    fprintf(stderr, "loaded image %s (%dx%d)\n", argv[i], s->w, s->h);
+  }
+  
+  // init ncurses
   initscr();
-	printw("Hello World !!!");
 	refresh();
-	int c = getch();
-	// addch(c);
-	refresh();
+
+  SDL_Rect src;
+  src.w = imgs[0]->w;
+  src.h = imgs[0]->h;
+  src.x = 0;
+  src.y = 0;
+
+  int c;
+
+  SDL_BlitSurface(imgs[0], &src, gsurf, NULL);
+  SDL_UpdateWindowSurface(gwin);
+    
+  while(true)
+  {
+    c = getch();
+    move(0,0);
+    clrtoeol();
+    if(c == 'k')
+      src.y -= 10;
+    if(c == 'j')
+      src.y += 10;
+    if(c == 'l')
+      src.x += 10;
+    if(c == 'h')
+      src.x -= 10;
+    if(c == 'q')
+      break;
+    
+    printw("%d %d", src.x, src.y);
+    
+    SDL_FillRect( gsurf, NULL, SDL_MapRGB( gsurf->format, 0xFF, 0xFF, 0xFF ) );
+    SDL_BlitSurface(imgs[0], &src, gsurf, NULL);
+    SDL_UpdateWindowSurface(gwin);
+
+    
+  }
+
+  return 0;
+  
+	// end ncurses
 	getch();
 	endwin();
 
-	return 0;
-
-  // // initialize window
-  // init("tetrui", SCREEN_WIDTH, SCREEN_HEIGHT);
-  // SDL_FillRect( gsurf, NULL, SDL_MapRGB( gsurf->format, 0xFF, 0xFF, 0xFF ) );
-  // SDL_UpdateWindowSurface( gwin );
+  // end SDL2 window
+  close();
 
   return 0;
 }
